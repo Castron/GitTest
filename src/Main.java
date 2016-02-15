c831037d76eb7bbfcedc56a88395eccea36babed
Benjamin Christiansen <benjamin.ao.chr@gmail.com>
2016-02-15 15:39:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Benjamin on 15-02-2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is a simple test");
+
+        System.out.println("Here is a for loop counting to 10");
+
+        count();
+    }
+
+    public static void count(){
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
